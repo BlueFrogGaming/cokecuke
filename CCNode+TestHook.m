278d6c31ebda385b5003bb1eab0eb2630355fa1f
Jason Felice <jason.m.felice@gmail.com>
2011-01-04 16:58:07 -0500

    Fix coordinate transformations for non-portrait

--- CCNode+TestHook.m
+++ CCNode+TestHook.m
@@ -3,8 +3,13 @@
 @implementation CCNode (TestHook)
 
 - (void)appendFrameToXml:(NSMutableString *)xml {
-	CGRect frame = [self boundingBox];
-	frame.origin = [[self parent] convertToWorldSpace:frame.origin];
+	CGRect frame = CGRectMake(0, 0, self.contentSizeInPixels.width, self.contentSizeInPixels.height);
+	CGAffineTransform t = [self nodeToWorldTransform];
+	frame = CGRectApplyAffineTransform(frame, t);
+	frame = CC_RECT_PIXELS_TO_POINTS(frame);
+	
+	frame = [[[CCDirector sharedDirector] openGLView] convertRect:frame toView:nil];
+	frame = [[[UIApplication sharedApplication] keyWindow] convertRect:frame toWindow:nil];
 	
 	[xml appendFormat: @"<frame x=\"%f\" y=\"%f\" width=\"%f\" height=\"%f\"/>",
 	  frame.origin.x, frame.origin.y, frame.size.width, frame.size.height];	
